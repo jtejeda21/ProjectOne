f0b8d39ebe37478ac0e3ce0305b1ef2152c39a98
jtejeda21 <jtejeda21@gmail.com>
2016-06-08 18:58:07 -0700

    repostion boxes added button

--- Hangman_Game/js/javman.js
+++ Hangman_Game/js/javman.js
@@ -1,4 +1,21 @@
-var alphaBet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+//to do:
+//player enters letter guess and clicks enter button
+//enter button
 
-var wordWin = ['W','A','R','R','I','O','R','S']
+//load game at start up
+window.onload = function() {
+	// alert('PLAYER 1, YOU\'RE UP!') //on load prompts player 1
 
+
+var alphaBet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+var wordWin = ['W','A','R','R','I','O','R','S'];
+
+
+var letterBox = document.getElementById('guess-button');
+	for (var i = 0; i < alphaBet.length; i++) {
+
+	}
+
+
+};
